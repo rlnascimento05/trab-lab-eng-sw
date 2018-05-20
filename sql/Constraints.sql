e8ab303ada403f2354c8a0149df1ec9a8451778a
lucario4545 <nikolasstacioni@gmail.com>
2018-05-20 19:37:39 -0300

    Update Constraints.sql
    Adicionando as FK's

--- sql/Constraints.sql
+++ sql/Constraints.sql
@@ -4,3 +4,8 @@ ALTER TABLE Pedido 		  ADD CONSTRAINT pk_Pedido 		  PRIMARY KEY (CodPedido);
 ALTER TABLE Pagamento 	ADD CONSTRAINT pk_Pagamento 	PRIMARY KEY (CodPagamento);
 ALTER TABLE Medicamento ADD CONSTRAINT pk_Medicamento	PRIMARY KEY (CodMedicamento);
 ALTER TABLE Cosmetico 	ADD CONSTRAINT pk_Cosmetico 	PRIMARY KEY (CodCosmetico);
+ 
+ALTER TABLE Pedido      ADD CONSTRAINT fk_Pedido        FOREIGN KEY (cpf)         REFERENCES CPF(Cliente);
+ALTER TABLE Pagamento   ADD CONSTRAINT fk_Pagamento     FOREIGN KEY (codpedido)   REFERENCES CodPedido(Pedido);
+ALTER TABLE Medicamento ADD CONSTRAINT fk_Medicamento   FOREIGN KEY (cpf)         REFERENCES CPF(Funcionario);
+ALTER TABLE Cosmeticos  ADD CONSTRAINT fk_Cosmeticos    FOREIGN KEY (cpf)         REFERENCES CPF(Funcionario);
