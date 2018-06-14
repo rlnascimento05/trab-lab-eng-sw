b24b72739414e1fe0a9e144a3b1d8b00a9d7ed96
ErickRD <erickrd_telt@hotmail.com>
2018-06-13 22:08:01 -0300

    Update ClienteDAO.java
    Update array
    update get
    arraylist

--- src/br/farmacia/dao/ClienteDAO.java
+++ src/br/farmacia/dao/ClienteDAO.java
@@ -2,7 +2,10 @@ package br.farmacia.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import br.farmacia.database.ConnectionFactory;
 import br.farmacia.database.DatabaseConfig;
@@ -22,7 +25,7 @@ public class ClienteDAO {
         try (Connection conn = this.connector.getConnection()) {
             ps = (PreparedStatement) conn.prepareStatement(
 
-                    "insert into cliente("
+                    "insert into Cliente("
                     + "Nome, "
                     + "CPF, "
                     + "Sexo, "
@@ -32,7 +35,7 @@ public class ClienteDAO {
                     + "values(?,?,?,?,?,?)");
 
             ps.setString(1, cliente.getNome());
-            ps.setInt(2, cliente.getCpf());
+            ps.setInt(2, cliente.getCPF());
             ps.setString(3, cliente.getSexo());
             ps.setString(4,cliente.getEmail());
             ps.setInt(5,cliente.getCelular());
@@ -47,6 +50,14 @@ public class ClienteDAO {
         return returnCode;
     }
 
+	public int insert(ArrayList<Cliente> clientes){
+    	int returnCode = 0;
+    	for(Cliente cli : clientes){
+    		returnCode += this.insert(cli);
+    	}
+		return returnCode;
+    }
+	
     public int delete(Cliente cliente) {
         java.sql.PreparedStatement ps;
         int returnCode = 0;
@@ -73,17 +84,18 @@ public class ClienteDAO {
                 + "Email = ?, "
                 + "Celular = ?, "
                 + "Telefone = ? "
-                + "where cpf = ?");
+                + "where CPF = ?");
 
             ps.setString(1, cliente.getNome());
-            ps.setInt(2, cliente.getCpf());
+            ps.setInt(2, cliente.getCPF());
             ps.setString(3, cliente.getSexo());
             ps.setString(4,cliente.getEmail());
             ps.setInt(5,cliente.getCelular());
             ps.setInt(6,cliente.getTelefone());
-            ps.setInt(7, cliente.getCpf());
+            ps.setInt(7, cliente.getCPF());
 
-            returnCode = ps.executeUpdate();
+            conn.close();
+            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -91,8 +103,55 @@ public class ClienteDAO {
 
     }
 
-    public Cliente getCliente(int cpf){
+    public int update(Cliente[] clientes){
+        int returnCode = 0;
+        for (Cliente cliente : clientes){
+            returnCode +=this.update(cliente);
+        }
+        return returnCode;
+    }
+
+    public Cliente getCliente(int idCliente){
+    	Cliente cli = null;
+        java.sql.PreparedStatement ps;
+        try (Connection conn = this.connector.getConnection()) {
+        	ps = conn.prepareStatement ("select * from cliente where CodCliente = ?");
+            ps.setInt(2,idCliente);
+
+            ResultSet result = ps.executeQuery();
+
+            ArrayList<Cliente> cliente = buildClientes(result);
+            cli = cliente.get(0);
+
+            conn.close();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cli;
+    }
+
+    private ArrayList<Cliente> buildClientes(ResultSet result) throws SQLException{
+        ArrayList<Cliente> clis = new ArrayList<Cliente>();
+        while (result.next()){
+            Cliente cli = new Cliente();
+            cli.setNome(result.getString("Nome"));           
+            cli.setCPF(result.getInt("CPF"));
+			cli.setSexo(result.getString("Sexo"));
+			cli.setEmail(result.getString("Email"));
+			cli.setCelular(result.getInt("Celular"));
+			cli.setTelefone(result.getInt("Telefone"));
+			
+            clis.add(cli);
+        }
+
+        return clis;
+    }
+
+
+    public ArrayList<Cliente> getAllClientes(){
         //TODO: Finish this thing
+        java.sql.PreparedStatement ps;
         return null;
     }
 }
