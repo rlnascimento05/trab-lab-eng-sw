1462929ae4c754b76ccee99b90f8c07a8270b7d0
Ishiilindo <gui.ishii@gmail.com>
2018-06-13 22:36:46 -0300

    Modificações nos Cosmeticos feitas por Ishii

--- src/br/farmacia/modelo/Cosmetico.java
+++ src/br/farmacia/modelo/Cosmetico.java
@@ -1,14 +1,43 @@
 package br.farmacia.modelo;
 
 public class Cosmetico extends Produto {
+    private int codCosmetico;
     private String marca;
     private String uso;
 
-    public Cosmetico() {
-        // TODO Auto-generated constructor stub
+    public Cosmetico(){
+    	
+    }
+    
+    public Cosmetico(int codCosmetico, String marca, String uso) {
+        this.codCosmetico = codCosmetico;
+        this.marca = marca;
+        this.uso = uso;
     }
 
     //TODO: Getters and Setters
 
+    public String getMarca() {
+        return marca;
+	}
 
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getUso() {
+        return uso;
+    }
+
+    public void setUso(String uso) {
+        this.uso = uso;
+    }
+
+    public int getCodCosmetico() {
+        return codCosmetico;
+    }
+
+    public void setCodMedicamento(int codCosmetico) {
+        this.codCosmetico = codCosmetico;
+    }
 }
