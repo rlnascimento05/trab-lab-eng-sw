517310ffcc89b8282a745d16fc3d9d83bbcae7c3
ErickRD <erickrd_telt@hotmail.com>
2018-06-13 21:25:21 -0300

    Update Constraints.sql
    Add tabela produto
    Add constraints pedido, medicamento , cosmeticos

--- sql/Constraints.sql
+++ sql/Constraints.sql
@@ -4,8 +4,12 @@ ALTER TABLE Pedido      ADD CONSTRAINT pk_Pedido        PRIMARY KEY (CodPedido);
 ALTER TABLE Pagamento   ADD CONSTRAINT pk_Pagamento     PRIMARY KEY (CodPagamento);
 ALTER TABLE Medicamento ADD CONSTRAINT pk_Medicamento   PRIMARY KEY (CodMedicamento);
 ALTER TABLE Cosmetico   ADD CONSTRAINT pk_Cosmetico     PRIMARY KEY (CodCosmetico);
+ALTER TABLE Produto     ADD CONSTRAINT pk_Produto       PRIMARY KEY (codproduto);
  
 ALTER TABLE Pedido      ADD CONSTRAINT fk_Pedido        FOREIGN KEY (cpf)         REFERENCES CPF(Cliente);
 ALTER TABLE Pagamento   ADD CONSTRAINT fk_Pagamento     FOREIGN KEY (codpedido)   REFERENCES CodPedido(Pedido);
 ALTER TABLE Medicamento ADD CONSTRAINT fk_Medicamento   FOREIGN KEY (cpf)         REFERENCES CPF(Funcionario);
 ALTER TABLE Cosmeticos  ADD CONSTRAINT fk_Cosmeticos    FOREIGN KEY (cpf)         REFERENCES CPF(Funcionario);
+ALTER TABLE Pedido      ADD CONSTRAINT fk_Pedido        FOREIGN KEY (codproduto)  REFERENCES codproduto(Produto);
+ALTER TABLE Medicamento ADD CONSTRAINT fk_Medicamento   FOREIGN KEY (codproduto)  REFERENCES codproduto(Produto);
+ALTER TABLE Cosmeticos  ADD CONSTRAINT fk_Cosmeticos    FOREIGN KEY (codproduto)  REFERENCES codproduto(Produto);
